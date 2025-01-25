f58589fe4f4ab7ed464d46bf4e72186ab13d434e
cp-20 <n.naoki.1587@gmail.com>
2025-01-25 19:25:17 +0900

    fix(normalizer): not return error

--- normalizer/query_normalizer.go
+++ normalizer/query_normalizer.go
@@ -8,8 +8,8 @@ import "regexp"
 var inRegex = regexp.MustCompile(`IN\s+\((\?,\s*)+\?\)`)
 var valuesRegex = regexp.MustCompile(`VALUES\s+\((\?,\s*)+\?\)`)
 
-func NormalizeQuery(query string) (string, error) {
+func NormalizeQuery(query string) string {
 	query = inRegex.ReplaceAllString(query, "IN (?)")
 	query = valuesRegex.ReplaceAllString(query, "VALUES (?)")
-	return query, nil
+	return query
 }
