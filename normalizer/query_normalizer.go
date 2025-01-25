e0b9f250275d979408029c6cc97cfa725e5d4c9d
cp20 <47262658+cp-20@users.noreply.github.com>
2025-01-25 19:23:41 +0900

    fix(normalizer): use regex based normalizer instead of parser based one (#32)

--- normalizer/query_normalizer.go
+++ normalizer/query_normalizer.go
@@ -1,115 +1,15 @@
 package normalizer
 
-import (
-	"fmt"
-
-	"github.com/traP-jp/h24w-17/sql_parser"
-)
+import "regexp"
 
 // IN (?, ?, ?) -> IN (?)
 // VALUES (?, ?, ?) -> VALUES (?)
-// select id from table -> SELECT `id` FROM `table`
+
+var inRegex = regexp.MustCompile(`IN\s+\((\?,\s*)+\?\)`)
+var valuesRegex = regexp.MustCompile(`VALUES\s+\((\?,\s*)+\?\)`)
 
 func NormalizeQuery(query string) (string, error) {
-	parsed, err := sql_parser.ParseSQL(query)
-	if err != nil {
-		return query, fmt.Errorf("failed to parse sql: %w", err)
-	}
-	transformed, err := normalizeSQLNode(parsed)
-	if err != nil {
-		return query, fmt.Errorf("failed to transform sql: %w", err)
-	}
-	return transformed.String(), nil
-}
-
-func normalizeSQLNode(node sql_parser.SQLNode) (out sql_parser.SQLNode, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			out = node
-			err = fmt.Errorf("panic: %v", r)
-		}
-	}()
-
-	// TODO: generalize select, update, and delete statements
-	switch n := node.(type) {
-	case sql_parser.SelectStmtNode:
-		if n.Conditions == nil {
-			return n, nil
-		}
-		transformed, err := normalizeSQLNode(*n.Conditions)
-		if err != nil {
-			return n, fmt.Errorf("failed to transform conditions: %w", err)
-		}
-		conditions := transformed.(sql_parser.ConditionsNode)
-		n.Conditions = &conditions
-		return n, nil
-	case sql_parser.UpdateStmtNode:
-		if n.Conditions == nil {
-			return n, nil
-		}
-		transformed, err := normalizeSQLNode(*n.Conditions)
-		if err != nil {
-			return n, fmt.Errorf("failed to transform conditions: %w", err)
-		}
-		conditions := transformed.(sql_parser.ConditionsNode)
-		n.Conditions = &conditions
-		return n, nil
-	case sql_parser.InsertStmtNode:
-		transformed, err := normalizeSQLNode(n.Values)
-		if err != nil {
-			return n, fmt.Errorf("failed to transform values: %w", err)
-		}
-		n.Values = transformed.(sql_parser.ValuesNode)
-		return n, nil
-	case sql_parser.DeleteStmtNode:
-		if n.Conditions == nil {
-			return n, nil
-		}
-		transformed, err := normalizeSQLNode(*n.Conditions)
-		if err != nil {
-			return n, fmt.Errorf("failed to transform conditions: %w", err)
-		}
-		conditions := transformed.(sql_parser.ConditionsNode)
-		n.Conditions = &conditions
-		return n, nil
-	case sql_parser.ConditionsNode:
-		transformed := make([]sql_parser.ConditionNode, 0, len(n.Conditions))
-		for _, c := range n.Conditions {
-			visited, err := normalizeSQLNode(c)
-			if err != nil {
-				return n, err
-			}
-			transformed = append(transformed, visited.(sql_parser.ConditionNode))
-		}
-		n.Conditions = transformed
-		return n, nil
-	case sql_parser.ConditionNode:
-		if n.Operator.Operator != sql_parser.Operator_IN {
-			return n, nil
-		}
-		val := n.Value.(sql_parser.ValuesNode)
-		if len(val.Values) == 0 {
-			return n, fmt.Errorf("unexpected value length: %d", len(val.Values))
-		}
-		// look at the first one only (for simplicity)
-		// false positive case: (?, 1, 2) -> (?)
-		if val.Values[0].String() != "?" {
-			return n, nil
-		}
-		val.Values = []sql_parser.SQLNode{sql_parser.PlaceholderNode{}}
-		n.Value = val
-		return n, nil
-	case sql_parser.ValuesNode:
-		if len(n.Values) == 0 {
-			return n, fmt.Errorf("unexpected value length: %d", len(n.Values))
-		}
-		// look at the first one only (for simplicity)
-		// false positive case: (?, 1, 2) -> (?)
-		if n.Values[0].String() != "?" {
-			return n, nil
-		}
-		n.Values = []sql_parser.SQLNode{sql_parser.PlaceholderNode{}}
-		return n, nil
-	}
-	return node, nil
+	query = inRegex.ReplaceAllString(query, "IN (?)")
+	query = valuesRegex.ReplaceAllString(query, "VALUES (?)")
+	return query, nil
 }
--- normalizer/query_normalizer_test.go
+++ normalizer/query_normalizer_test.go
@@ -12,20 +12,16 @@ func TestNormalizeQuery(t *testing.T) {
 		expected string
 	}{
 		{
-			query:    "select id from table;",
-			expected: "SELECT `id` FROM `table`;",
+			query:    "SELECT `id` from `table`",
+			expected: "SELECT `id` from `table`",
 		},
 		{
-			query:    "insert into table (name,col) values (?, ?);",
-			expected: "INSERT INTO `table` (`name`, `col`) VALUES (?);",
+			query:    "INSERT INTO table (name, col) VALUES (?, ?)",
+			expected: "INSERT INTO table (name, col) VALUES (?)",
 		},
 		{
-			query:    "update table set id = ? order by id desc limit 1 offset ?;",
-			expected: "UPDATE `table` SET `id` = ? ORDER BY `id` DESC LIMIT 1 OFFSET ?;",
-		},
-		{
-			query:    "delete from table where id in (?, ?, ?, ?);",
-			expected: "DELETE FROM `table` WHERE `id` IN (?);",
+			query:    "DELETE FROM `table` WHERE id IN (?, ?, ?, ?);",
+			expected: "DELETE FROM `table` WHERE id IN (?);",
 		},
 	}
 	for _, tt := range tests {
