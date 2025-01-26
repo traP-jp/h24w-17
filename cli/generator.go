e688be889ac4b94f389cf6b18462eeda1d7d3351
cp-20 <n.naoki.1587@gmail.com>
2025-01-26 14:20:20 +0900

    fix(cli): generate

--- cli/generator.go
+++ cli/generator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 	h24w17 "github.com/traP-jp/h24w-17"
@@ -23,7 +24,16 @@ var generateCmd = &cobra.Command{
 		}
 		distPath := args[0]
 
-		g := h24w17.NewGenerator(plan, schema)
+		planContent, err := readFile(plan)
+		if err != nil {
+			return fmt.Errorf("error reading plan file: %v", err)
+		}
+		schemaContent, err := readFile(schema)
+		if err != nil {
+			return fmt.Errorf("error reading schema file: %v", err)
+		}
+
+		g := h24w17.NewGenerator(planContent, schemaContent)
 		g.Generate(distPath)
 
 		return nil
@@ -34,4 +44,12 @@ func init() {
 	generateCmd.Flags().StringP("plan", "p", "isuc.yaml", "File containing the cache plan")
 	generateCmd.Flags().StringP("schema", "s", "schema.sql", "File containing the table schema")
 	rootCmd.AddCommand(generateCmd)
+}
+
+func readFile(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", fmt.Errorf("error reading file: %v", err)
+	}
+	return string(data), nil
 }
