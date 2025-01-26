9bc27263b706de20b49646421398685af99cbe77
cp20 <47262658+cp-20@users.noreply.github.com>
2025-01-26 12:22:42 +0900

    fix(normalizer): trim space (#43)

--- normalizer/query_normalizer.go
+++ normalizer/query_normalizer.go
@@ -12,6 +12,7 @@ var inRegex = regexp.MustCompile(`IN\s+\((\?,\s*)+\?\)`)
 var valuesRegex = regexp.MustCompile(`VALUES\s+\((\?,\s*)+\?\)`)
 
 func NormalizeQuery(query string) string {
+	query = strings.TrimSpace(query)
 	query = inRegex.ReplaceAllString(query, "IN (?)")
 	query = valuesRegex.ReplaceAllString(query, "VALUES (?)")
 	if !strings.HasSuffix(query, ";") {
