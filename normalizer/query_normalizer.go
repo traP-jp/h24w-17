1b441d790b8cc690bbcc00ec9a2cc5bafc61cdef
cp20 <47262658+cp-20@users.noreply.github.com>
2025-01-26 12:50:40 +0900

    chore(normalizer): normalize to no backquote (#45)

--- normalizer/query_normalizer.go
+++ normalizer/query_normalizer.go
@@ -5,18 +5,31 @@ import (
 	"strings"
 )
 
-// IN (?, ?, ?) -> IN (?)
-// VALUES (?, ?, ?) -> VALUES (?)
-
+var spaceRegex = regexp.MustCompile(`\s+`)
 var inRegex = regexp.MustCompile(`IN\s+\((\?,\s*)+\?\)`)
 var valuesRegex = regexp.MustCompile(`VALUES\s+\((\?,\s*)+\?\)`)
 
 func NormalizeQuery(query string) string {
+	// remove spaces
+	query = strings.ReplaceAll(query, "\r", " ")
+	query = strings.ReplaceAll(query, "\n", " ")
+	query = strings.ReplaceAll(query, "\t", " ")
+	query = spaceRegex.ReplaceAllString(query, " ")
 	query = strings.TrimSpace(query)
+
+	// remove backquotes
+	query = strings.ReplaceAll(query, "`", "")
+
+	// IN (?, ?, ?) -> IN (?)
 	query = inRegex.ReplaceAllString(query, "IN (?)")
+
+	// VALUES (?, ?, ?) -> VALUES (?)
 	query = valuesRegex.ReplaceAllString(query, "VALUES (?)")
+
+	// add semicolon
 	if !strings.HasSuffix(query, ";") {
 		query += ";"
 	}
+
 	return query
 }
